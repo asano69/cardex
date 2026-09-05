c4defda8772226641a8648cc215c8fdcef742bc2
asano69 <186772504+asano69@users.noreply.github.com>
2026-09-06 08:40:05 +0900

    chore

--- migrations/1788651594_collections_snapshot.go
+++ migrations/1788651594_collections_snapshot.go
@@ -1338,6 +1338,83 @@ func init() {
 				"type": "base",
 				"updateRule": null,
 				"viewRule": null
+			},
+			{
+				"createRule": null,
+				"deleteRule": null,
+				"fields": [
+					{
+						"autogeneratePattern": "[a-z0-9]{15}",
+						"help": "",
+						"hidden": false,
+						"id": "text3208210256",
+						"max": 15,
+						"min": 15,
+						"name": "id",
+						"pattern": "^[a-z0-9]+$",
+						"presentable": false,
+						"primaryKey": true,
+						"required": true,
+						"system": true,
+						"type": "text"
+					},
+					{
+						"cascadeDelete": true,
+						"collectionId": "pbc_2527524235",
+						"help": "",
+						"hidden": false,
+						"id": "relation1602912115",
+						"maxSelect": 0,
+						"minSelect": 0,
+						"name": "source",
+						"presentable": false,
+						"required": true,
+						"system": false,
+						"type": "relation"
+					},
+					{
+						"cascadeDelete": false,
+						"collectionId": "pbc_2527524235",
+						"help": "",
+						"hidden": false,
+						"id": "relation1181691900",
+						"maxSelect": 0,
+						"minSelect": 0,
+						"name": "target",
+						"presentable": false,
+						"required": true,
+						"system": false,
+						"type": "relation"
+					},
+					{
+						"hidden": false,
+						"id": "autodate2990389176",
+						"name": "created",
+						"onCreate": true,
+						"onUpdate": false,
+						"presentable": false,
+						"system": false,
+						"type": "autodate"
+					},
+					{
+						"hidden": false,
+						"id": "autodate3332085495",
+						"name": "updated",
+						"onCreate": true,
+						"onUpdate": true,
+						"presentable": false,
+						"system": false,
+						"type": "autodate"
+					}
+				],
+				"id": "pbc_449060851",
+				"indexes": [],
+				"listRule": null,
+				"name": "links",
+				"system": false,
+				"type": "base",
+				"updateRule": null,
+				"viewRule": null
 			}
 		]`
 
